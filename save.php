7c86b14ce5c0a382ae328ac5b123442266cd851f
AndresNM1979 <87467337+AndresNM1979@users.noreply.github.com>
2021-08-28 20:34:47 +0200

    cambiamos mysql por postgres

--- save.php
+++ save.php
@@ -13,8 +13,8 @@ error_reporting(0);
 $q1="CREATE TABLE $table( value VARCHAR(50000))";
 $q2="INSERT into $table VALUES('$v')";
 
-$sq1=mysqli_query($con,$q1);
-$sq2=mysqli_query($con,$q2);
+$sq1=postgresi_query($con,$q1);
+$sq2=postgresi_query($con,$q2);
 
 
 
@@ -23,9 +23,9 @@ if($sq1 && $sq2)
 {
 	$sqq="SELECT * FROM  $table";
 
-    $r=mysqli_query($con,$sqq);
+    $r=postgresi_query($con,$sqq);
 
-   /*while($row=mysqli_fetch_array($r))
+   /*while($row=postgresi_fetch_array($r))
    {
      echo("$row[value]");
    }*/
@@ -52,9 +52,9 @@ require_once("connection.php");
 
 $q6="SELECT table_name FROM information_schema.tables where table_schema='problem' ";
 
-$res=mysqli_query($con,$q6);
+$res=postgresi_query($con,$q6);
 
-while($row=mysqli_fetch_array($res))
+while($row=mpostgresi_fetch_array($res))
 {
 	error_reporting(0);
 	echo '<form action="test.php" method="POST"> <input type="hidden" name="pb" value="' . htmlspecialchars($row[table_name]) . '" /><input type="submit" value=" ' . htmlspecialchars($row[table_name]) .'  "/></form>'."<br>";
